5c4f914ceb0969f97ae889366042a1b078b3d2c7
K. M. Soriful Islam Adnan <adnanfaith7@gmail.com>
2022-11-11 09:01:10 +0600

    database created and secure username password

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -10,10 +12,16 @@ app.use(express.json());
 app.use(cors());
 
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3aa5vlu.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
 app.get('/', (req, res)=>{
     res.send('AKL Lawyer Service Running');
 });
 
 app.listen(port, ()=>{
-    
+
 });
